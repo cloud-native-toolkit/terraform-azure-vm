247ab38e64e19f02d79e1f1a87dd45e6182849dc
Sudhir Jain <sudhir.jain@ibm.com>
2022-05-18 12:30:33 +0530

    1 terraform azure vm module (#2)
    * Virtual Machine Terraform Module
    
    Signed-off-by:Sudhir Jain <SudhirJain@ibm.com>
    
    * Test Plan pending, complete module for virtual machine
    
    Signed-off-by:Sudhir Jain <SudhirJain@ibm.com>
    
    * Updates test stages with required input
    
    Signed-off-by:Sudhir Jain <SudhirJain@ibm.com>
    
    * Workflow script updates
    
    * Corrected workflow script
    
    * Workflow fix
    
    * Update stage2-vm.tf
    
    Signed-off-by:Sudhir Jain <SudhirJain@ibm.com>
    
    * Updated Stage 2-vm resource group name refrence
    
    Signed-off-by:Sudhir Jain <SudhirJain@ibm.com>
    
    * Applied Code Review changes as suggested
    
    Signed-off-by:Sudhir Jain <SudhirJain@ibm.com>
    
    * Removed the azure provider section from main.tf
    
    Sudhir Jain
    
    Co-authored-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

---
+++ main.tf
@@ -0,0 +1,48 @@
+resource "azurerm_network_interface" "network_interface" {
+  name                = var.network_interface_name
+  location            = var.region
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = var.ip_configuration_name
+    #Subnet id of existing subnet
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = var.private_ip_address_allocation
+  }
+}
+
+resource "azurerm_virtual_machine" "virtual_machine" {
+  name                  = var.virtual_machine_name
+  location              = var.region
+  resource_group_name   = var.resource_group_name
+  network_interface_ids = [azurerm_network_interface.network_interface.id]
+  vm_size               = var.vm_size
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = var.delete_data_disks_on_termination
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  delete_data_disks_on_termination = var.delete_data_disks_on_termination
+
+  storage_image_reference {
+    publisher = var.storage_image_reference_publisher
+    offer     = var.storage_image_reference_offer
+    sku       = var.storage_image_reference_sku
+    version   = var.storage_image_reference_version
+  }
+  storage_os_disk {
+    name              = var.storage_os_disk_name
+    caching           = var.storage_os_disk_caching
+    create_option     = var.storage_os_disk_create_option
+    managed_disk_type = var.storage_os_disk_managed_disk_type
+  }
+  os_profile {
+    computer_name  = var.os_profile_computer_name
+    admin_username = var.os_profile_admin_username
+    admin_password = var.os_profile_admin_password
+  }
+  os_profile_linux_config {
+    disable_password_authentication = var.os_profile_linux_config_disable_password_authentication
+  }
+
+}
--- variables.tf
+++ variables.tf
@@ -1,6 +1,107 @@
-#variable "myvariable" {
-#  type        = string
-#  description = "A description of my variable"
-#  default     = ""
-#}
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the Azure resource group where the NAT gateway has been provisioned"
+}
+
+variable "region" {
+  type        = string
+  description = "The Azure location where the NAT gateway will be installed"
+}
+variable "virtual_network_name" {
+  type        = string
+  description = "The Azure virtual network name"
+}
+variable "network_interface_name" {
+  type        = string
+  description = "The Azure location where the NAT gateway will be installed"
+}
+
+variable "ip_configuration_name" {
+  type        = string
+  description = "The Azure location where the NAT gateway will be installed"
+
+}
+variable "subnet_id" {
+  type        = string
+  description = "The Azure subnet id"
+}
+variable "private_ip_address_allocation" {
+  type        = string
+  description = "The Azure subnet private ip address alocation"
+  default     = "Dynamic"
+}
+variable "virtual_machine_name" {
+  type        = string
+  description = "This is the name of Azure VM"
+
+}
+variable "vm_size" {
+  type        = string
+  description = "This is the size of Virtual Machine"
+
+}
+variable "delete_os_disk_on_termination" {
+  type        = bool
+  description = "Flag to delete os disk on termination"
+  default     = true
+}
+
+variable "delete_data_disks_on_termination" {
+  type        = bool
+  description = "Flag to delete data disk on termination"
+  default     = true
+}
+variable "storage_image_reference_publisher" {
+  type        = string
+  description = "This is the storage image refrence publisher"
+}
+variable "storage_image_reference_offer" {
+  type        = string
+  description = "This is the storage image refrence offer"
+}
+variable "storage_image_reference_sku" {
+  type        = string
+  description = "This is the storage image refrence sku"
+}
+variable "storage_image_reference_version" {
+  type        = string
+  description = "This is the storage image refrence version"
+}
+#Storage OS Disk Vairables
+variable "storage_os_disk_name" {
+  type        = string
+  description = "This is the storage OS disk name"
+}
+variable "storage_os_disk_caching" {
+  type        = string
+  description = "This is the storage OS disk cashing"
+}
+variable "storage_os_disk_create_option" {
+  type        = string
+  description = "This is the storage OS disk create option"
+}
+variable "storage_os_disk_managed_disk_type" {
+  type        = string
+  description = "This is the storage OS disk managed disk type"
+}
+#OS Profile
+variable "os_profile_computer_name" {
+  type        = string
+  description = "This is the storage OS Profile Computer Name"
+}
+variable "os_profile_admin_username" {
+  type        = string
+  description = "This is the storage OS Profile Admin Username"
+}
+variable "os_profile_admin_password" {
+  type        = string
+  description = "This is the storage OS Profile Passowrd"
+  sensitive   = true
+}
+variable "os_profile_linux_config_disable_password_authentication" {
+  type        = bool
+  description = "Flag to disable password authentication"
+  default     = false
+
+}
 
