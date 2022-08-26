7e1760b129287b973b3a85b2504fd82ed452550f
rich-ehrhardt <rich_ehrhardt@au1.ibm.com>
2022-08-26 17:02:55 +1000

    Changed subnet cidr handling
    Signed-off-by: rich-ehrhardt <rich_ehrhardt@au1.ibm.com>

--- test/stages/stage1-subnet.tf
+++ test/stages/stage1-subnet.tf
@@ -1,10 +1,14 @@
+locals {
+  subnet_cidr = cidrsubnets(var.vnet_cidr, 2)
+}
+
 module "subnets" {
   source = "github.com/cloud-native-toolkit/terraform-azure-subnets"
 
   resource_group_name = module.resource_group.name
   region              = module.resource_group.region
   vnet_name           = module.vnet.name
-  ipv4_cidr_blocks    = [cidrsubnets(var.vnet_cidr, 2)]
+  ipv4_cidr_blocks    = ["${local.subnet_cidr}"]
   acl_rules = [{
     name        = "ssh-inbound"
     action      = "Allow"
